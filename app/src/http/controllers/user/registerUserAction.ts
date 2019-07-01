dcbe617673cfddb888b6719715460466c63501a0
Majid Akbari <majidakbariiii@gmail.com>
2019-07-01 19:45:09 +0430

    remove unwanted functions from registerAction

--- app/src/http/controllers/user/registerUserAction.ts
+++ app/src/http/controllers/user/registerUserAction.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from "express";
 import {userRepository} from "../../../repository/userRepository";
 import * as bcrypt from 'bcrypt';
-import {HTTP422Error} from "../../../utils/httpErrors";
 import {HttpSuccess} from "../../../utils/httpSuccess";
 
 /**
  * @class registerUserAction
  */
 export class registerUserAction {
-
 
     constructor(private repo: userRepository) {}
 
@@ -18,24 +16,10 @@ export class registerUserAction {
      */
     async invoke(req: Request, res: Response) {
         const data = req.body;
-        await this.checkUniqueEmail(data.email);
         data.password = await bcrypt.hash(data.password, 10);
         const user = await this.repo.create(data);
         user.password = undefined;
 
         return HttpSuccess(res, user, 201);
-    }
-
-
-    /**
-     *
-     * @param email
-     */
-    async checkUniqueEmail(email: String) {
-        if(await this.repo.findOneByEmail(email)) {
-            throw new HTTP422Error([{
-                'email' : 'Email should be unique'
-            }])
-        }
     }
 }
